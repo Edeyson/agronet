08b68207f9c30cc7b0eef1a4f4cce94aa7494bc3
Santiago Velez S <santivelezs@gmail.com>
2021-04-22 10:11:38 -0500

    locations migration

---
+++ Backend/app/Http/Controllers/Api/V1/Geo/GeographicLocationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Geo;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GeographicLocationController extends Controller
+{
+    //
+}
