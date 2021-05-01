65594715261a80c7b14882d8898214a5f13d92fd
Santiago Velez S <santivelezs@gmail.com>
2021-05-01 17:11:50 -0500

    div fron n back

--- Frontend02/src/app/models/post-model.ts
+++
@@ -1,7 +0,0 @@
-export interface PostModel
-{
-  data: {
-    type: string
-    attributes: { }
-  };
-}
--- Frontend02/src/app/models/token.ts
+++
@@ -1,6 +0,0 @@
-export interface Token{
-  token?: string;
-  slug: string; // TODO: codificar slug string
-  role_id: string;
-  message: string;
-}
