ed20db1044c60cb8d754e1f7b84c5f4da6a396c6
Edeyson <60797892+Edeyson@users.noreply.github.com>
2021-05-06 09:41:35 -0500

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   mapbox: {
-    accessToken: "pk.eyJ1IjoiZWRleXNvbiIsImEiOiJja285Ym9jd3YyM3B1MnVscnhsb3NvN28wIn0.HXXauXWFBuKgiQ25kmG5HA"
+    accessToken: ""
   }
 };
 
